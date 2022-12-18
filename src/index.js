0a15e215d4ada5989d81e30aad4a1470e8c0cd30
Zhanna Bohan <foto1drug@gmail.com>
2022-12-18 18:03:08 +0300

    feat: add up navigation and exit condition with ctrl+c and exit command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
-import { argv } from "node:process";
+import { argv, cwd, chdir, stdin, stdout, exit } from "node:process";
+import { homedir } from "node:os";
+import * as readline from "readline/promises";
+
+chdir(homedir());
 
 const args = argv.slice(2);
 const argName = String(args).split("=")[1];
 const userName = argName !== "your_username" ? argName : "Anonymous";
-console.log(`Welcome to the File Manager, ${userName}!`);
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+const startWorkingDirectory = homedir();
+console.info(`Starting working directory ${startWorkingDirectory}`);
+
+console.info(`Welcome to the File Manager, ${userName}!`);
+
+console.info(`You are currently in ${startWorkingDirectory}`);
+
+const goUpFromCurrentDirectory = () => {
+  try {
+    chdir("..");
+    console.info(`You are currently in ${cwd()}`);
+  } catch (err) {
+    console.error("Operation failed");
+  }
+};
+
+rl.on("line", async (data) => {
+  if (data === "up") goUpFromCurrentDirectory();
+  if (data === "exit") {
+    console.log(`Thank you for using File Manager, ${userName}!`);
+    setTimeout(() => exit(), 0);
+  }
+})
+  .on("SIGINT", () => rl.close())
+  .on("close", () =>
+    console.log(`Thank you for using File Manager, ${userName}!`)
+  )
+  .on("error", (err) => console.error(err));
